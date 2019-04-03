af71ef5551f8dd476fbd642cc388e909988030ab
James Cox <jamesncox@yahoo.com>
2019-04-03 15:58:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    new_array = []
+    array.select do |word|
+      if word.split(" ").sort == @word.split(" ").sort
+        new_array << @word
+      end
+      binding.pry
+    end
+    new_array
+  end
+  
+  # def match(array)
+  #   %w(array)
+  #   binding.pry
+  # end
+  
+end
